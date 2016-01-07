d434c3b17fc9997891840a8cc94089529b0e9a3f
Yani0029 <yani@bellcom.dk>
2016-01-07 16:41:16 +0100

    Added script in subsite_create.sh to fix permissions after new site is created

--- scripts/subsite_create.sh
+++ scripts/subsite_create.sh
@@ -48,3 +48,35 @@ set_permissions
 add_to_crontab
 add_subsiteadmin
 #mail_status "$SITENAME" "mmh@bellcom.dk"
+
+#
+# FROM PUPPET. DONT EDIT
+# Try to set correct permissions for files
+#
+
+PWD=`pwd`
+PATHARRAY=( `echo ${PWD} | tr '/' ' '`)
+
+if [[ ${PATHARRAY[1]} != 'www'  ]]; then
+  exit 10
+fi
+
+chgrp --preserve-root -R www-data "$PWD"
+chmod --preserve-root -R ug+rwX "$PWD"
+chmod --preserve-root -R o-w "$PWD"
+find "$PWD" -type d -exec chmod g+s '{}' \;
+
+# Set correct permissions for Drupal
+if [ -d $PWD"/public_html/sites/default" ]; then
+  chmod --preserve-root a-w $PWD"/public_html/sites/default"
+fi
+if [ -d $PWD"/sites/default" ]; then
+  chmod --preserve-root a-w $PWD"/sites/default"
+fi
+if [ -f $PWD"/public_html/sites/default/settings.php" ]; then
+  chmod --preserve-root a-w $PWD"/public_html/sites/default/settings.php"
+fi
+if [ -f $PWD"/sites/default/settings.php" ]; then
+  chmod --preserve-root a-w $PWD"/sites/default/settings.php"
+fi
+
