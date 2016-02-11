517bc54d2f0c1b6469a67d0991d993d659e6c9dd
Franz Skaaning <fs@bellcom.dk>
2016-02-11 15:24:51 +0100

    Take 3

--- public_html/profiles/os2sub/themes/svendborg_subsitetheme/theme/node--os2web-spotbox-box.tpl.php
+++ public_html/profiles/os2sub/themes/svendborg_subsitetheme/theme/node--os2web-spotbox-box.tpl.php
@@ -15,7 +15,9 @@
       <div class="table">
         <div class="table-row">
           <div class="table-cell gradient-lightgreen">
-            <h3 class="panel-title "><?php print render($content['field_os2web_spotbox_text']); ?></h3>
+            <div>
+              <h3 class="panel-title "><?php print render($content['field_os2web_spotbox_text']); ?></h3>
+            </div>
           </div>
         </div>
       </div>
