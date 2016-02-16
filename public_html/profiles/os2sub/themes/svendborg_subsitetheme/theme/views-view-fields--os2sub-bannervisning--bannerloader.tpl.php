4b16d3b6841958fe627114a75bb140747d00cc56
Franz Skaaning <fs@bellcom.dk>
2016-02-16 15:03:31 +0100

    Tekst i banner på undersider

--- public_html/profiles/os2sub/themes/svendborg_subsitetheme/theme/views-view-fields--os2sub-bannervisning--bannerloader.tpl.php
+++ public_html/profiles/os2sub/themes/svendborg_subsitetheme/theme/views-view-fields--os2sub-bannervisning--bannerloader.tpl.php
@@ -1,6 +1,13 @@
 <div class="page-calendar-slider ">
-<div class="slider-cover single'" style="background-image: url('<?php print $fields['field_os2web_base_field_banner']->content; ?>')">
-<?php print $fields['field_os2web_base_isproject_desc']->content; ?>
-</div></div>
-
-
+  <div class="slider-cover single" style="background-image: url('<?php print $fields['field_os2web_base_field_banner']->content; ?>')">
+    <div class="container">
+      <div class="row">
+        <div class="text-center">
+          <div class="title ">
+            <?php print $fields['field_os2web_base_isproject_desc']->content; ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
