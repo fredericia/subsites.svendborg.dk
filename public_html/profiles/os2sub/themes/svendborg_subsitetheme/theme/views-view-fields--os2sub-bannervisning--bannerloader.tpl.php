dd1e94e39d276cdbc996c9088576d18e4fce7ef2
Franz Skaaning <fs@bellcom.dk>
2016-02-16 14:51:51 +0100

    Rem overlay

--- public_html/profiles/os2sub/themes/svendborg_subsitetheme/theme/views-view-fields--os2sub-bannervisning--bannerloader.tpl.php
+++ public_html/profiles/os2sub/themes/svendborg_subsitetheme/theme/views-view-fields--os2sub-bannervisning--bannerloader.tpl.php
@@ -1,12 +1,6 @@
 <div class="page-calendar-slider ">
 <div class="slider-cover single'" style="background-image: 
--moz-linear-gradient(left, rgba(0,0,0,0.75) 0%, rgba(0,0,0,0.75) 100%),  url('<?php print $fields['field_os2web_base_field_banner']->content; ?>');
-background-image: -webkit-gradient(left top, right top, color-stop(0%, rgba(0,0,0,0.75)), color-stop(100%, rgba(0,0,0,0.75))), url('<?php print $fields['field_os2web_base_field_banner']->content; ?>');
-background-image: -webkit-linear-gradient(left, rgba(0,0,0,0.75) 0%, rgba(0,0,0,0.75) 100%), url('<?php print $fields['field_os2web_base_field_banner']->content; ?>');
-background-image: -o-linear-gradient(left, rgba(0,0,0,0.75) 0%, rgba(0,0,0,0.75) 100%), url('<?php print $fields['field_os2web_base_field_banner']->content; ?>');
-background-image: -ms-linear-gradient(left, rgba(0,0,0,0.75) 0%, rgba(0,0,0,0.75) 100%), url('<?php print $fields['field_os2web_base_field_banner']->content; ?>');
-background-image: linear-gradient(to right, rgba(0,0,0,0.75) 0%, rgba(0,0,0,0.75) 100%), url('<?php print $fields['field_os2web_base_field_banner']->content; ?>');
-filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#000000', endColorstr='#000000', GradientType=1 );'">
+-moz-linear-gradient(left, rgba(0,0,0,0.75) 0%, rgba(0,0,0,0.75) 100%),  url('<?php print $fields['field_os2web_base_field_banner']->content; ?>')">
 <?php print $fields['field_os2web_base_isproject_desc']->content; ?>
 </div></div>
 
