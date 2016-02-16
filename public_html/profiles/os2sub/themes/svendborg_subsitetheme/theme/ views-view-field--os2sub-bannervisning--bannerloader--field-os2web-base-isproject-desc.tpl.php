268d2db0a30c76bf8089cae5bb2c5bc3d5a739b2
Franz Skaaning <fs@bellcom.dk>
2016-02-16 13:29:20 +0100

    Banner tpl

---
+++ public_html/profiles/os2sub/themes/svendborg_subsitetheme/theme/ views-view-field--os2sub-bannervisning--bannerloader--field-os2web-base-isproject-desc.tpl.php
@@ -0,0 +1,11 @@
+<div class="page-calendar-slider ">
+<div class="slider-cover single'" style="background-image: 
+-moz-linear-gradient(left, rgba(0,0,0,0.75) 0%, rgba(0,0,0,0.75) 100%),  url('[field_os2web_base_field_banner]');
+background-image: -webkit-gradient(left top, right top, color-stop(0%, rgba(0,0,0,0.75)), color-stop(100%, rgba(0,0,0,0.75))), url('[field_os2web_base_field_banner]');
+background-image: -webkit-linear-gradient(left, rgba(0,0,0,0.75) 0%, rgba(0,0,0,0.75) 100%), url('[field_os2web_base_field_banner]');
+background-image: -o-linear-gradient(left, rgba(0,0,0,0.75) 0%, rgba(0,0,0,0.75) 100%), url('[field_os2web_base_field_banner]');
+background-image: -ms-linear-gradient(left, rgba(0,0,0,0.75) 0%, rgba(0,0,0,0.75) 100%), url('[field_os2web_base_field_banner]');
+background-image: linear-gradient(to right, rgba(0,0,0,0.75) 0%, rgba(0,0,0,0.75) 100%), url('[field_os2web_base_field_banner]');
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#000000', endColorstr='#000000', GradientType=1 );'">
+[field_os2web_base_isproject_desc]
+</div></div>
