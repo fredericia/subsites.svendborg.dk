f8ec65b0e718ed889bf06f1c63feb67d9596c83a
Franz Skaaning <fs@bellcom.dk>
2016-02-16 14:53:43 +0100

    Debugging bg overlay

--- public_html/profiles/os2sub/themes/svendborg_subsitetheme/theme/views-view-fields--os2sub-bannervisning--bannerloader.tpl.php
+++ public_html/profiles/os2sub/themes/svendborg_subsitetheme/theme/views-view-fields--os2sub-bannervisning--bannerloader.tpl.php
@@ -1,6 +1,5 @@
 <div class="page-calendar-slider ">
-<div class="slider-cover single'" style="background-image: 
--moz-linear-gradient(left, rgba(0,0,0,0.75) 0%, rgba(0,0,0,0.75) 100%),  url('<?php print $fields['field_os2web_base_field_banner']->content; ?>')">
+<div class="slider-cover single'" style="background-image: url('<?php print $fields['field_os2web_base_field_banner']->content; ?>')">
 <?php print $fields['field_os2web_base_isproject_desc']->content; ?>
 </div></div>
 
